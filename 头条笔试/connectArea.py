5434b55992b16705203550621262f58f0cc6ae70
unknown <309625784@qq.com>
2018-08-12 16:19:48 +0800

    优化算法

--- 头条笔试/connectArea.py
+++ 头条笔试/connectArea.py
@@ -4,7 +4,7 @@
 首先遍历数组，遇到一个“1”就将其标记为当前的flag值（flag从2开始）。
 然后将1周围的8邻域遍历，寻找有没有值为“1”的，如果有，那么将其标记为flag，并调用search。
 """
-
+"""
 def isRoad(row, col, M, N):
     if row < M and row >=0 and col>= 0 and col < N:
         return True
@@ -60,9 +60,8 @@ def main(M, N, array):
                 num = search(i, j, M, N, array, flag, num)
                 count.append(num)
                 flag += 1
-
+    print(array)
     return count
-
 
 M = 10
 N = 10
@@ -83,6 +82,65 @@ matrix = [
 connects = main(M, N, matrix)
 print("球队个数： ", len(connects))
 print("数量最多的球队： ", max(connects))
+
+"""
+
+def solve(row, col, array):
+    if array[row][col] == 0:
+        return 0
+    count = 1
+    array[row][col] = 0
+    if row + 1 < len(array):
+        count += solve(row + 1, col, array)
+    if col + 1 < len(array[0]):
+        count += solve(row, col+1, array)
+    if row - 1 >= 0:
+        count += solve(row-1, col, array)
+    if col - 1 >= 0:
+        count += solve(row, col - 1, array)
+
+    if row - 1 >= 0 and col - 1 >= 0:
+        count += solve(row - 1, col - 1, array)
+    if row + 1 < len(array) and col + 1 < len(array[0]):
+        count += solve(row + 1, col + 1, array)
+    if row + 1 < len(array) and col - 1 >= 0:
+        count += solve(row + 1, col - 1, array)
+    if row - 1 >= 0 and col + 1 < len(array[0]):
+        count += solve(row - 1, col + 1, array)
+    return count
+
+
+def main(M, N, array):
+    countNum = 0
+    maxCount = 0
+    for i in range(M):
+        for j in range(N):
+            if array[i][j] == 1:
+                count = solve(i, j, array)
+                if count > maxCount:
+                    maxCount = count
+                countNum += 1
+    return countNum, maxCount
+
+
+M = 10
+N = 10
+matrix = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0, 1, 1],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+num, numMax = main(M, N, matrix)
+print("球队个数： ", num)
+print("数量最多的球队： ", numMax)
 
 
 
